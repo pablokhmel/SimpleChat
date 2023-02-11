54505557a86a09cab56ac6f2dac76cab638a0abb
pablokhmel <pablokhmelevskiy@gmail.com>
2023-02-11 20:07:42 +0100

    Initial Commit

---
+++ SimpleChat/SimpleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatApp.swift
+//  SimpleChat
+//
+//  Created by MacBook on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
